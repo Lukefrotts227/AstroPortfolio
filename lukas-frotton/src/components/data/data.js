84959f6ea8d4fa670f8427c44eaedf7678a7996e
Ljf227 <ljfrotton@gmail.com>
2023-11-21 12:51:32 -0500

    starting the transfer process

---
+++ lukas-frotton/src/components/data/data.js
@@ -0,0 +1,133 @@
+export const aboutMe = "My name is Lukas and I am a Computer Science major and currently a sophmore at Montclair State University. "
++ "I love coding in my free time and am always looking to create new projects. To hone my skills, I have and will continue to attend "
++ "hackathons. I love learning new technologies, frameworks, skills and languages. I am always willing to test out something new at "
++ "at a hackathon, or for my own personal projects."
+
+
+export const projects = [
+    {
+        name: "Song Assosation Game", 
+        mini_description:  "Connecting friends through shared song",
+        description: "We built a game that prompts the user with a word. Then they must sing a song including this word. "
+        + "It then detects if the word was sang.",
+        how: "Kivy, Speech Recognition, Pyaudio",
+        future: "Improve speech reconginiton, add additional functionality and add song detection",
+        image: "./song-association.png",
+        where: "https://devpost.com/software/song-association-game",
+        
+        
+    },
+    {
+        name: "Climate Collab", 
+        mini_description:  "Combat climate change through collaboration",
+        description: "A simple carbon calculator that combines the data of multiple individuals to push progress on a macro scale.",
+        how: "React, MongoDB Atlas, Flask",
+        future: "Improve the carbon emssion calculation algorithim, improve the user interface, add more functionality "
+        + "and add more social aspects",
+        image: "./climate-collab.jpg",
+        where: "https://devpost.com/software/carbon-footprint-collab",
+    }, 
+
+    {
+        name: "Community Organizer", 
+        mini_description: "Bringing communities together",
+        description: "A  social media app that communities so they can organize local political action.",
+        how: "React, Fastapi, MongoDB atlas, OpenAI",
+        future: "Imporve user interface, add more traditional social media features, include an sql database, transistion to the "
+        + "django framework",
+        image: "./community-organizer.jpg",
+        where: "https://devpost.com/software/community-organizer", 
+    },
+];
+
+
+export const miniProjects = [
+    {
+        name: "C++ Hashtable",
+        description: "A custom hashtable that supports resizing, custom data types and custom hashing functions",
+        how: "C++",
+        future: "Add greater functionlaity and find ways to make it more unique and applicable",
+        where: "https://github.com/Lukefrotts227/learningC/tree/Main/datastructures/hashtablecpp"
+    },
+
+    {
+        name: "C++ Linked List", 
+        description: "A custom doubly linked list that supports all forms of insertion, search and deletion. It also includes a duplicate remover",
+        how: "C++",
+        future: "Add more functionality and use more operator classes",
+        where: "https://github.com/Lukefrotts227/learningC/tree/Main/datastructures/linkedlistcpp"
+    },
+]
+
+export const skills = [
+    {name: "Python",
+    description: "The language I have the most expereince in, I have done coursework and large projects with it",
+    },
+
+    {name: "Java",
+    description: "The majority of my coursework historically has been in this programming language",
+    }, 
+
+    {name: "C/C++",
+    description: "I have recently learned these check out my github to see my progress",
+    },
+
+    {name: "MongoDB",
+    description: "I have used MongoDB as my database for multiple projects",
+    },
+
+    {name: "React",
+    description: "I have used this framework for multiple user interface including this user interface",
+    },
+
+    {name: "Html/Css/Js",
+    description: "The basics of web development on the frontend I continue to better my skills with these",
+    },
+
+    {name: "Express",
+    description: "I have used this to create backends for web applications",
+    },
+
+    {name: "Flask", 
+    description: "I have used this frameowrk to code backend for web applications",
+    },
+]
+
+export const courses = [
+    {
+        name: "Java programming II",
+        description: "In this course we delved deeper into the world of java programming. We covered topics "
+        + "such as object oriented programming, inheritence and polymorphism.",
+    },
+    {
+        name: "Computer Systems",
+        description: "In this course we covered the underlying systems of how computers operate. This included covering logic circuits, registers "
+        + "and memory. We then moved into basic assembly programming with the MIPS architecture.",
+    }, 
+
+    {
+        name: "Introduction to python programming", 
+        description: "In this course we covered the basics of python programming. This includes the basics of control flow"
+        + ", loops and functions."
+    },
+]
+
+export const next_description = "I will continue to work on this portfolio. Furthermore, I will be attending multiple hackathons in the future " 
++ "including HackRU and HackHarvard"; 
+
+
+
+
+
+export const learning = [
+    {
+        name: "rust",
+        description: "",
+    },
+
+    {
+        name: "SQL",
+        description: "",
+    },
+
+]
